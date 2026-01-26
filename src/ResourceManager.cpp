87e77e496ddee0d737aa345e60f186760dfbbca6
velo <Velociraptor1!>
2026-01-26 17:46:27 +0100

    Restore the texture format

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -672,6 +672,7 @@ void ResourceManager::createSkyboxImage(const Scene& scene) {
     
     // Skybox texture: limit mip levels (keep original format - may be HDR 16-bit)
     texture.mipLevels = 1;
+    texture.format = vk::Format::eR16G16B16A16Unorm;
 
     m_imageManager.createImageFromTexture(
         texture,
